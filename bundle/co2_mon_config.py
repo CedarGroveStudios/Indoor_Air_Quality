e321097de7d5eeb6bf7671d792d178caacf5927e
Cedar Grove Maker Studios <jan@goolsbey.net>
2021-08-13 14:27:51 -0700

    initial repo load

---
+++ bundle/co2_mon_config.py
@@ -0,0 +1,12 @@
+# co2_mon_config.py
+# ### Alarm and range default values in PPM ###
+ALARM_CO2 = 2500
+
+CO2_GOOD = 0
+CO2_POOR = 1000
+CO2_WARNING = 2000
+CO2_DANGER = 5000
+
+CO2_MAXIMUM = 6000  # Maximum scale display value
+
+SENSOR_INTERVAL = 30  # Interval between measurements (2 to 1800 seconds)
