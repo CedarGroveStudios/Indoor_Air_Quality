56b8d48bb8f83bcd8c557b1ee52265e2b5713230
Cedar Grove Maker Studios <jan@goolsbey.net>
2021-09-20 11:17:02 -0700

    remove unit converter .mpy files (incompatible with CPy v7.0.0)

---
+++ bundle/cedargrove_unit_converter/air_quality/interpreter/english_to_deutsch.py
@@ -0,0 +1,35 @@
+# english_to_deutsch.py
+# English to Deutsch (German)
+# Thank you to @effiksmusic for the translation!
+
+ENG_DEUTSCH = {
+    "Air Quality": "Luftqualität",
+    "ALARM": "ALARM",
+    "Alarm": "Alarm",
+    "CALIBRATE": "KALIBRIEREN",
+    "DANGER": "GEFAHR",
+    "ENGLISH": "DEUTSCH",
+    "GOOD": "GUT",
+    "HAZARDOUS": "GEFÄHRLICH",
+    "Indoor Air Quality": "Raumluftqualität",
+    "INVALID": "UNGÜLTIG",
+    "LANGUAGE": "SPRACHE",
+    "LOW BATTERY": "BATTERIE LEER",
+    "MODERATE": "MÄSSIG",
+    "OVERRANGE": "ÜBER MAXIMUM",
+    "POOR": "SCHLECHT",
+    "SENSITIVE": "SENSIBEL",
+    "TEMPERATURE": "TEMPERATUR",
+    "UNHEALTHY": "UNGESUND",
+    "V UNHEALTHY": "SEHR UNGESUND",
+    "WARMUP": "VORBEREITUNG",
+    "WARNING": "WARNUNG",
+}
+
+
+def interpret(enable, english_phrase):
+    # returns translated phrase or original phrase
+    if enable:
+        if english_phrase in ENG_DEUTSCH:
+            return ENG_DEUTSCH[english_phrase]
+    return english_phrase
